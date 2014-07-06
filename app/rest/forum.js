7081d635cb7abb4326b00bef8c9718977c42ad45
Sumit Arora <er.sumitarora@gmail.com>
2014-07-05 20:55:24 -0400

    seperating routes

---
+++ app/rest/forum.js
@@ -0,0 +1,11 @@
+exports.index = function(req, res){
+  res.send('forum index');
+};
+
+exports.new = function(req, res){
+  res.send('new forum');
+};
+
+exports.create = function(req, res){
+  res.send('create forum');
+};
--- server.js
+++ server.js
@@ -1,47 +1,37 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express    = require('express');
 var bodyParser = require('body-parser');
+//var resource   = require('express-resource');
 var app        = express();
 
-// configure app
 app.use(bodyParser());
 
 var port     = process.env.PORT || 8080; // set our port
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
-var Bear     = require('./models/bear');
+var Bear     = require('./app/models/bear');
+var forum     = require('./app/rest/forum');
 
-// ROUTES FOR OUR API
-// =============================================================================
+//app.resource('forums', require('./app/rest/forum'));
 
-// create our router
 var router = express.Router();
 
-// middleware to use for all requests
 router.use(function(req, res, next) {
-  // do logging
   console.log('Something is happening.');
   next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/forum', forum.index);
+
 router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// on routes that end in /bears
-// ----------------------------------------------------
 router.route('/bears')
-
-  // create a bear (accessed at POST http://localhost:8080/bears)
   .post(function(req, res) {
 
-    var bear = new Bear();    // create a new instance of the Bear model
-    bear.name = req.body.name;  // set the bears name (comes from the request)
+    var bear = new Bear();
+    bear.name = req.body.name;
 
     bear.save(function(err) {
       if (err)
@@ -53,7 +43,6 @@ router.route('/bears')
 
   })
 
-  // get all the bears (accessed at GET http://localhost:8080/api/bears)
   .get(function(req, res) {
     Bear.find(function(err, bears) {
       if (err)
@@ -63,11 +52,8 @@ router.route('/bears')
     });
   });
 
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
 router.route('/bears/:bear_id')
 
-  // get the bear with that id
   .get(function(req, res) {
     Bear.findById(req.params.bear_id, function(err, bear) {
       if (err)
@@ -76,7 +62,6 @@ router.route('/bears/:bear_id')
     });
   })
 
-  // update the bear with this id
   .put(function(req, res) {
     Bear.findById(req.params.bear_id, function(err, bear) {
 
@@ -94,7 +79,6 @@ router.route('/bears/:bear_id')
     });
   })
 
-  // delete the bear with this id
   .delete(function(req, res) {
     Bear.remove({
       _id: req.params.bear_id
@@ -107,10 +91,7 @@ router.route('/bears/:bear_id')
   });
 
 
-// REGISTER OUR ROUTES -------------------------------
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
